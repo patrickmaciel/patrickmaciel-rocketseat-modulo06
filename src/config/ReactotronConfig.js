2c812d3f2d8a6776ab7648ed129583fbd17e2647
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-28 08:08:50 -0300

    Listando favoritos

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,4 +1,5 @@
 import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const tron = Reactotron.configure({
@@ -7,6 +8,7 @@ if (__DEV__) {
     port: 9090,
   })
     .useReactNative()
+    .setAsyncStorageHandler(AsyncStorage)
     .connect();
 
   console.tron = tron;
